607e9658fb38953614a6b0419d00b6633e773ee5
Lars Djerf <lars.djerf@gmail.com>
2013-09-11 10:20:34 +0200

    Add license to setup.py.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import setup, Extension
 setup(name="msgq",
       version="0.1",
       description="System V IPC Message Queue Python Extension Module",
+      license="GPLv3",
       author="Lars Djerf",
       author_email="lars.djerf@gmail.com",
       url="http://github.com/sral/msgq",
